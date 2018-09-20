80d3c37a4043214a0539eb24b7c712daff0b1c77
Antonio Ponce <alponce@uninorte.edu.co>
2018-09-20 09:19:29 -0500

    arreglo del JQ

--- js/index.js
+++ js/index.js
@@ -1,7 +1,7 @@
 $(function(){
-    //$('.programa-evento .info-curso').hide();
+    $('.programa-evento .info-curso').hide();
     $('.programa-evento .info-curso:first').show();
-    $('.programa-evento .info-curso:first').addClass('activo');
+    $('.menu-programa a:first').addClass('activo');
     $('.menu-programa a').on('click',function(){
         $('.menu-programa a').removeClass('activo');
         $(this).addClass('activo');
